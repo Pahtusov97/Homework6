5817ff43e271d59503e9191cf3e9d084ccfa8f7c
Pahtusov97 <pahtusovgsh@yandex.ru>
2024-07-04 16:33:22 +0300

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,14 @@
+my_dict = {"Alexandr" : 1997, "Mary" : 1996}
+print(my_dict)
+print(my_dict["Mary"])
+print(my_dict.get("Oleg"))
+my_dict.update({"Egor" : 1987,
+                "Artem" : 1999})
+my_dict.pop("Alexandr")
+print(my_dict)
+my_set = {1,1,2,2,3,3,4,4,5,5,3,3,2,2,5,5,4,4,1,1,1}
+print(my_set)
+print(my_set.add(10,))
+print(my_set.add("Home"))
+print(my_set.discard(3))
+print(my_set)
